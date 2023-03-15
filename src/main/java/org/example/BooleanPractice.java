0181b582cdeb5303b0d7b1285c671044e1f0a635
Alexandra <a.soroka@geekbrains.team>
2023-03-15 21:16:27 +0300

    Пройден 9ый урок из курса лекций по java

---
+++ src/main/java/org/example/BooleanPractice.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class BooleanPractice {
+    public static void main(String[] args) {
+//        String text = "";
+//        boolean empty = text.isEmpty();
+//        System.out.println(empty);
+//        text = "blablabla";
+//        empty = text.isEmpty();
+//        System.out.println(empty); //Логическая еременная работает логично)))
+        String text = "txt";
+        print(text);
+        text = "";
+        print(text);
+        text = "control";
+        print(text);
+
+        System.out.println(division(4,2));
+        System.out.println(division(0, 1));
+        System.out.println(division(4,0));
+    }
+    private static void print(String str){
+        if (!str.isEmpty()){
+            System.out.println(str);
+        }
+    }
+
+    private static int division(int num1, int num2){
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return 911;
+    }
+}
